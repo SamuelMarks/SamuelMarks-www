6a64a756a8e6b477e4d5be335535d971a45ac54b
Samuel Marks <samuelmarks@gmail.com>
2018-05-01 03:57:55 +1000

    [feat] Responsive homepage with flex-layout and media queries

--- src/app/slash/slash.module.ts
+++ src/app/slash/slash.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialImportModule } from '../material-import/material-import.module';
 import { SvgViewerModule } from '../svg-viewer/svg-viewer.module';
@@ -8,9 +9,10 @@ import { SvgViewerModule } from '../svg-viewer/svg-viewer.module';
 import { SlashComponent } from './slash.component';
 import { slashRoutes } from './slash.routes';
 
+
 @NgModule({
   imports: [
-    CommonModule, RouterModule, RouterModule.forChild(slashRoutes),
+    CommonModule, RouterModule, RouterModule.forChild(slashRoutes), FlexLayoutModule,
     MaterialImportModule, SvgViewerModule
   ],
   declarations: [SlashComponent]
