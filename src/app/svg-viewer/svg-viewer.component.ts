60050c9360b33ce3e98ac79c63e8431e230bcc84
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-06-13 15:02:48 +1000

    [package.json,package-lock.json] Upgrade dependencies; bump version (MAJOR: Angular 8); [src/polyfill.ts,src/app/svg-viewer] Angular 8; [README,LICENSE-MIT,LICENSE-APACHE,package.json] Added license; [app/learn-engineering/learn-engineering.html] Added textbook section

--- src/app/svg-viewer/svg-viewer.component.ts
+++ src/app/svg-viewer/svg-viewer.component.ts
@@ -1,19 +1,17 @@
 import { Component, ElementRef, Input, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 
 const getAbsolutePathFromSrc = (src: string) => src.slice(src.indexOf('assets/') - 1);
 
 @Component({
   selector: 'app-svg-viewer',
-  template: '<div class="app-svg-viewer" aria-hidden="true"></div>',
-  styleUrls: ['./svg-viewer.component.css']
+  template: '<div class="docs-svg-viewer" aria-hidden="true"></div>',
 })
 export class SvgViewerComponent implements OnInit {
   @Input() src: string;
   @Input() scaleToContainer: boolean;
 
-  constructor(private elementRef: ElementRef, private http: Http) {
-  }
+  constructor(private elementRef: ElementRef, private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchAndInlineSvgContent(this.src);
@@ -32,8 +30,8 @@ export class SvgViewerComponent implements OnInit {
 
   private fetchAndInlineSvgContent(path: string): void {
     const svgAbsPath = getAbsolutePathFromSrc(path);
-    this.http.get(svgAbsPath).subscribe(svgResponse =>
-      this.inlineSvgContent(svgResponse.text())
-    );
+    this.http.get(svgAbsPath, {responseType: 'text'}).subscribe(svgResponse => {
+      this.inlineSvgContent(svgResponse);
+    });
   }
 }
--- src/app/svg-viewer/svg-viewer.module.ts
+++ src/app/svg-viewer/svg-viewer.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SvgViewerComponent } from './svg-viewer.component';
 
+
 @NgModule({
-  imports: [
-    CommonModule, HttpModule
-  ],
+  imports: [HttpClientModule],
+  exports: [SvgViewerComponent],
   declarations: [SvgViewerComponent],
-  exports: [SvgViewerComponent]
 })
-export class SvgViewerModule {
-}
+export class SvgViewerModule {}
