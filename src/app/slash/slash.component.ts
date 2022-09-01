704ccd6b9a60bea807c84dce814d1e706d30e744
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2022-08-31 23:31:15 -0400

    [src/app/slash/slash.component.html] Use GitHub.com CSS (github-markdown-light.css) ; [src/app/slash/slash.component.ts] Include GitHub.com CSS ; [src/app/slash/github-markdown-light.css <- src/app/slash/gh-markdown.css] Replace with vanilla from https://github.com/sindresorhus/github-markdown-css

--- src/app/slash/slash.component.ts
+++ src/app/slash/slash.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-slash',
   templateUrl: './slash.component.html',
-  styleUrls: ['./slash.component.scss', './gh-markdown.css']
+  styleUrls: ['./slash.component.scss', './github-markdown-light.css']
 })
 export class SlashComponent implements OnInit {
 
