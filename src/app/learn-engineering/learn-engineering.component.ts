6bbe5d9676841b004d2e8e58970508f657f3c88b
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-03-09 23:50:19 -0500

    [src/app/learn-engineering/learn-engineering.component{.html,.scss,.ts}] Add a nice timeline theming (and new titles) for the learning-engineering route ; [{package-lock,package}.json] Upgrade dependencies

--- src/app/learn-engineering/learn-engineering.component.ts
+++ src/app/learn-engineering/learn-engineering.component.ts
@@ -3,7 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-learn-engineering',
   templateUrl: './learn-engineering.component.html',
-  styleUrls: ['./../getting-started/getting-started.component.css', './learn-engineering.component.css']
+  styleUrls: ['./../getting-started/getting-started.component.css', './learn-engineering.component.scss']
 })
 export class LearnEngineeringComponent implements OnInit {
 
